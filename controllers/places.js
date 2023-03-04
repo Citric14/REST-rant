1e93728cd34970e3463d4159902bcd2b2ea4b87b
George <georgehender997@gmail.com>
2023-03-04 16:35:06 -0500

    mid p7

--- controllers/places.js
+++ controllers/places.js
@@ -15,6 +15,19 @@ router.get('/:id', (req, res) => {
     }
   })
   
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id] })
+    }
+  })
+  
 
 router.get('/new', (req, res) => {
     res.render('places/new')
